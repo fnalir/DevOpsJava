f65338a255d353f17e1f2fa14b71f7b83c18a31e
Rahman <fn.alirahman@gmail.com>
2023-06-17 14:35:00 +0700

    1

---
+++ src/main/java/com/devops/devopsjava/DevOpsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.devops.devopsjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevOpsJavaApplication.class, args);
+    }
+
+}
